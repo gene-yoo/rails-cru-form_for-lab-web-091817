40a0332e4143dc74c2e3076369a0024ff1e4fd5f
gene-yoo <gene.y.yoo@gmail.com>
2017-10-16 17:15:26 -0400

    Lab complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Test Artist 1", bio: "New artist test 123")
+Genre.create(name: "HipHop")
+Song.create(name: "Song 1", artist_id: 1, genre_id: 1)
+
+Artist.create(name: "Test Artist 2", bio: "New artist test 456")
+Genre.create(name: "R&B")
+Song.create(name: "Song 2", artist_id: 2, genre_id: 2)
